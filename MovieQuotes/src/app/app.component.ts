29d711a7793b9466f4d261c5272ee57a54e010c4
Coolone36 <michaeljlaritz@gmail.com>
2017-09-30 23:00:14 -0400

    Finished local only movie quotes(no edit/delete)

--- MovieQuotes/src/app/app.component.ts
+++ MovieQuotes/src/app/app.component.ts
@@ -13,15 +13,23 @@ interface MovieQuote {
 export class AppComponent {
   movie = 'The Flash';
 
-  movieQuote: MovieQuote = {
+  formMovieQuote: MovieQuote = {
     'quote': '',
     'movie': '',
   };
+
+  movieQuotes: Array<MovieQuote> = [
+    { "movie": "Rocky", "quote": "Yo Adrian" },
+    { "movie": "Terminator", "quote": "I'll be back" },
+    { "movie": "Titanic", "quote": "I'm the king of the world!" },
+    { "movie": "The Princess Bride", "quote": "Hello. My name is Inigo Montoya. You killed my father. Prepare to die." }
+  ];
+
   onSubmit(): void {
-    console.log("TODO: You wish to submit:", this.movieQuote);
-    this.movieQuote = {
-    'quote': '',
-    'movie': '',
-  };
+    this.movieQuotes.unshift(this.formMovieQuote);
+    this.formMovieQuote = {
+      'quote': '',
+      'movie': '',
+    };
   }
 }
