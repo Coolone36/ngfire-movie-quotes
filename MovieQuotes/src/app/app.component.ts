d91da36d906479a7efe8c83ce60e91b7d0307b40
Coolone36 <michaeljlaritz@gmail.com>
2017-09-30 23:35:46 -0400

    Added write/read of firebase

--- MovieQuotes/src/app/app.component.ts
+++ MovieQuotes/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
 
 interface MovieQuote {
   movie: string;
   quote: string;
+  $key?: string;
 }
 
 @Component({
@@ -12,29 +14,38 @@ interface MovieQuote {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  movie = 'The Flash';
+  readonly quotesPath = 'quotes';
 
   formMovieQuote: MovieQuote = {
     'quote': '',
     'movie': '',
   };
 
-  movieQuotes: Array<MovieQuote> = [
-    { "movie": "Rocky", "quote": "Yo Adrian" },
-    { "movie": "Terminator", "quote": "I'll be back" },
-    { "movie": "Titanic", "quote": "I'm the king of the world!" },
-    { "movie": "The Princess Bride", "quote": "Hello. My name is Inigo Montoya. You killed my father. Prepare to die." }
-  ];
+  // movieQuotes: Array<MovieQuote> = [
+  //   { "movie": "Rocky", "quote": "Yo Adrian" },
+  //   { "movie": "Terminator", "quote": "I'll be back" },
+  //   { "movie": "Titanic", "quote": "I'm the king of the world!" },
+  //   { "movie": "The Princess Bride", "quote": "Hello. My name is Inigo Montoya. You killed my father. Prepare to die." }
+  // ];
 
+  // Read action into Firebase
+  movieQuotesStream: FirebaseListObservable<MovieQuote[]>;
   constructor(db: AngularFireDatabase) {
-
+    this.movieQuotesStream = db.list(this.quotesPath);
   }
 
   onSubmit(): void {
-    this.movieQuotes.unshift(this.formMovieQuote);
+    // Local only solution
+    // this.movieQuotesStream.unshift(this.formMovieQuote);
+    try { 
+    // Write action into Firebase
+    this.movieQuotesStream.push(this.formMovieQuote);
     this.formMovieQuote = {
       'quote': '',
       'movie': '',
     };
+    } catch (e) {
+      console.log("Form error: ", e);
+    }
   }
 }
